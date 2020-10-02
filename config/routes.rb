10980c311a7f9a3f28c3b38a923db958de947369
jeff <zombie1864@gmail.com>
2020-10-02 15:30:09 -0400

    started a new project using rails and bulma

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :entries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "entries#index" # entries#index is taken from controller#Action 
 end
