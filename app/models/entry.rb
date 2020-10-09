4fafcc6c2928fa7d336f7175becfe8fd71076a28
jeff <zombie1864@gmail.com>
2020-10-08 20:34:34 -0400

    added valifations to the entry form

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
-
+    validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: true # this adds validations to the field so that a person cannot submit an empty form 
     def day
         self.created_at.strftime('%b %e, %Y')
     end
