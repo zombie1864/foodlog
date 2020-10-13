a504faca342d1b930928dbbc9f3e05c7f6272867
jeff <zombie1864@gmail.com>
2020-10-13 12:53:27 -0400

    completed the ruby on rails crash course 2020 by traversy media

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ApplicationRecord
     belongs_to :category
-    validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: true # this adds validations to the field so that a person cannot submit an empty form 
+    validates :calories, :proteins, :carbohydrates, :fats, :meal_type, :category_id, presence: true # this adds validations to the field so that a person cannot submit an empty form 
     def day
         self.created_at.strftime('%b %e, %Y')
     end
